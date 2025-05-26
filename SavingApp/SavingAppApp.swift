a8299cbaf6f854cf4b78fd1c34219d71a4affa69
Mora Hakim <morahakim22@gmail.com>
2025-05-26 10:14:16 +0700

    Initial Commit

---
+++ SavingApp/SavingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingAppApp.swift
+//  SavingApp
+//
+//  Created by Mora on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SavingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
